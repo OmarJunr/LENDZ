bb3d570ff456720ffd351b276583fb23512e614d
Mateus Lazarotto <mateuslazarotto25@hotmail.com>
2022-06-27 02:11:57 -0300

    LZ17

--- src/componentsAdm/DaysSelects/index.tsx
+++ src/componentsAdm/DaysSelects/index.tsx
@@ -6,20 +6,35 @@ import { styles } from './styles';
 import { useNavigation } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
 
-type Props ={
-    daySelected:number;
+type Props = {
+    daySelected: number;
 }
 
-export function DaysSelects({daySelected}:Props) {
+export function DaysSelects({ daySelected }: Props) {
     const navigation = useNavigation();
-    const [check1, setCheck1] = useState('');
-    const [check2, setCheck2] = useState('');
-    const [check3, setCheck3] = useState('');
+    let [day1, day2, day3] = '';
 
+    if (daySelected != undefined) {
+        if (daySelected == 1) {
+            day1 = 'check'
+        }
+        if (daySelected == 2) {
+            day2 = 'check'
+        }
+        if (daySelected == 3) {
+            day3 = 'check'
+        }
+    }
 
+    const [check1, setCheck1] = useState(day1);
+    const [check2, setCheck2] = useState(day2);
+    const [check3, setCheck3] = useState(day3);
+    
     console.log(daySelected)
+
     return (
         <View style={styles.container}>
+
             <Text style={styles.textDay}>
                 Dias:
             </Text>
